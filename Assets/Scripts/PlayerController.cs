0d8fdfa8f45c0a4220468ad288e6a72373450b09
Cobalt1140 <hubert.twarowski@outlook.com>
2024-11-23 15:33:25 +0100

    remis le animateur

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,8 @@ public class PlayerController : MonoBehaviour
     public Sonar sonar3;
     public Sonar sonar4;
     public Sonar sonar5;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
 
     public bool timerRunning = false;
 
@@ -24,6 +26,17 @@ public class PlayerController : MonoBehaviour
 
         // Calculer le déplacement
         Vector3 movement = new Vector3(moveX, moveY, 0f);
+        animator.SetFloat("moveY", moveY);
+        animator.SetBool("isWalking", Mathf.Abs(moveX) + Mathf.Abs(moveY) != 0);
+        animator.SetBool("isWalkSide", moveX != 0);
+        if (moveX < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (moveX > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
 
         // Appliquer le déplacement au personnage
         transform.position += movement * speed * Time.deltaTime;
