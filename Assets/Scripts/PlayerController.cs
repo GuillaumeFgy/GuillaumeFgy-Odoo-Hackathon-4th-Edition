73278768909f56c1228b437c84146a27172e51bf
Cobalt1140 <hubert.twarowski@outlook.com>
2024-11-23 13:14:50 +0100

    finished Animator

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public AudioSource source;
     public float timerDuration = 2f; // Time in seconds
     private float timeRemaining;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
 
     public bool timerRunning = false;
 
@@ -18,9 +20,20 @@ public class PlayerController : MonoBehaviour
 
         // Calculer le déplacement
         Vector3 movement = new Vector3(moveX, moveY, 0f);
+        animator.SetFloat("moveY", moveY);
+        animator.SetBool("isWalking", Mathf.Abs(moveX)+Mathf.Abs(moveY)!=0);
+        animator.SetBool("isWalkSide", moveX!=0);
+        if (moveX < 0)
+        {
+            spriteRenderer.flipX = true;
+        } else if (moveX > 0) 
+        {
+            spriteRenderer.flipX= false;
+        }
 
         // Appliquer le déplacement au personnage
         transform.position += movement * speed * Time.deltaTime;
+
 
         TimerBip();
     }
