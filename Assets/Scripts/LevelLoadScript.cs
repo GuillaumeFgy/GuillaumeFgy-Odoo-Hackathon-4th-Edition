14742afc00d77386a62f90fe83471413acf13182
GuillaumeFgy <92818751+GuillaumeFgy@users.noreply.github.com>
2024-11-24 10:35:56 +0100

    crash transition

--- Assets/Scripts/LevelLoadScript.cs
+++ Assets/Scripts/LevelLoadScript.cs
@@ -6,6 +6,9 @@ public class LevelLoadScript : MonoBehaviour
 {
 
     public Animator transition;
+    public AudioSource audioSource;
+    public int duration = 1;
+    private bool played = false;
 
     public void LoadNextLevel()
     {
@@ -15,8 +18,13 @@ public class LevelLoadScript : MonoBehaviour
     IEnumerator LoadLevel(int levelIndex) 
     {
         transition.SetTrigger("Start");
+        if (levelIndex == 1 && !played) 
+        {
+            played = true;
+            audioSource.Play();
+        }
 
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(duration);
 
         SceneManager.LoadScene(levelIndex);
     }
